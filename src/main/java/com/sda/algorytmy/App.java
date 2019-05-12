9df9dbf7b73b9a1172416621616478a30e765295
ciechann <emgasuk@gmail.com>
2019-05-12 12:33:41 +0100

    while

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,22 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj dwie liczby calkowite: ");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            }
+            b = b - a;
+        }
+        System.out.println(a);
     }
 }
